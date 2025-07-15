3fe3103f07602f790cbc045d5dcc257c27335bed
SannikovVladislav <sannikovladislav@mail.ru>
2025-07-16 00:41:46 +0300

    Made UITableViewCell

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  Untitled.swift
+//  ImageFeed
+//
+//  Created by Владислав on 15.07.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+}
