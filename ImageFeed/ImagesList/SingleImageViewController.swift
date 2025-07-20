9ebd9ca2e5b7ff83ad2a49ab77d670fb30accd8c
SannikovVladislav <sannikovladislav@mail.ru>
2025-07-20 13:32:06 +0300

    Added Navigation

---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Владислав on 19.07.2025.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBAction private func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            imageView.image = image
+        }
+}
