57e01d839509e92b9aee8cbc3c2a87c878767a67
SannikovVladislav <sannikovladislav@mail.ru>
2025-07-31 22:12:13 +0300

    Added enum Constants

---
+++ ImageFeed/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Владислав on 31.07.2025.
+//
+import Foundation
+
+enum Constants {
+    static let accesKey = "KnQbhJO_cURlC40Ks5dYaL4fYk_FH52eh8SJncsB6eo"
+    static let secretKey = "0r-FcCABEWWQsINji5BK71oAZ9Tz590cwglCDXlXkiE"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")
+}
