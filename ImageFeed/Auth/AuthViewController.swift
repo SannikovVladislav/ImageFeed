8a89d2286d1eeeb13ed199f43b962b08c2e505a0
SannikovVladislav <sannikovladislav@mail.ru>
2025-08-07 00:02:38 +0300

    Added WebViewViewControllerDelegate

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -8,11 +8,25 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     private let showWebViewSegueIdentifier = "ShowWebView"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureBackButton()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else {
+                assertionFailure("Failed to prepare for \(showWebViewSegueIdentifier)")
+                return
+            }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
     
     private func configureBackButton() {
@@ -21,10 +35,14 @@ final class AuthViewController: UIViewController {
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black (iOS)")
     }
-    
-    @IBAction func loginButtonTapped(_ sender: UIButton) {
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
     }
-    
-    
-    
+
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        vc.dismiss(animated: true)
+    }
 }
--- ImageFeed/Helpers/Constants.swift
+++ ImageFeed/Helpers/Constants.swift
@@ -7,7 +7,7 @@
 import Foundation
 
 enum Constants {
-    static let accesKey = "KnQbhJO_cURlC40Ks5dYaL4fYk_FH52eh8SJncsB6eo"
+    static let accessKey = "KnQbhJO_cURlC40Ks5dYaL4fYk_FH52eh8SJncsB6eo"
     static let secretKey = "0r-FcCABEWWQsINji5BK71oAZ9Tz590cwglCDXlXkiE"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
