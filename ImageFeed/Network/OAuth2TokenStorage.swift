c84f14318bfc5e8c0a1497d5485e1d0f9f97ebc4
SannikovVladislav <sannikovladislav@mail.ru>
2025-08-25 17:44:55 +0300

    Added KeychainWrapper

--- ImageFeed/Network/OAuth2TokenStorage.swift
+++ ImageFeed/Network/OAuth2TokenStorage.swift
@@ -6,16 +6,27 @@
 //
 
 import Foundation
+import SwiftKeychainWrapper
 
 final class OAuth2TokenStorage {
-    private let tokenKey = "accessToken"
+    let tokenKey = "accessToken"
     
     var token: String? {
         get {
-            UserDefaults.standard.string(forKey: tokenKey)
+            KeychainWrapper.standard.string(forKey: tokenKey)
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: tokenKey)
+            if let newValue = newValue {
+                let isSuccess = KeychainWrapper.standard.set(newValue, forKey: tokenKey)
+                if !isSuccess {
+                    assertionFailure(("❌ Failed to save token in Keychain"))
+                }
+            } else {
+                let isSucceess = KeychainWrapper.standard.removeObject(forKey: tokenKey)
+                if !isSucceess {
+                    assertionFailure("❌ Failed to delete token in Keychain")
+                }
+            }
         }
     }
 }
